8edda9bae514041a07108bbb78b08053ad05366e
nice <wilmo@foxmail.com>
2018-11-26 16:53:49 +0800

    Update FeignServerApplication.java

--- feign-server/src/main/java/com/example/feignserver/FeignServerApplication.java
+++ feign-server/src/main/java/com/example/feignserver/FeignServerApplication.java
@@ -3,12 +3,15 @@ package com.example.feignserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @EnableDiscoveryClient
@@ -18,10 +21,14 @@ public class FeignServerApplication {
 
 	@Autowired
 	DiscoveryClient client;
+	@Autowired
+	Registration registration;
 
 	@RequestMapping("/upload")
-	public String upload() {
-		return "";
+	public ServiceInstance serviceInstance() {
+		List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+		System.out.println("ip:" + list.get(0).getHost() + ",port:" + list.get(0).getPort());
+		return null;
 	}
 
 	public static void main(String[] args) {
